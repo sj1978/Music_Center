1cf3805e00c5169662a0a699cff4acb49f823e39
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-08-09 20:10:31 -0300

    add line_items files and func

---
+++ db/migrate/20220809192859_add_quantity_to_line.rb
@@ -0,0 +1,5 @@
+class AddQuantityToLine < ActiveRecord::Migration[7.0]
+  def change
+    add_column :line_items, :quantity, :integer, default: 1
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_185137) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_192859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_185137) do
     t.integer "quantity"
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "instrument_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["instrument_id"], name: "index_line_items_on_instrument_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_185137) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "instruments"
 end
