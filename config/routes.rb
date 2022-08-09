cfcf3d0da45129b20fd6b3143d444e092abeb492
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-08-08 23:33:15 -0300

    devise files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-resources :guitars   
-resources :basses
+resources :instruments   
+
   # Defines the root path route ("/")
   root "home#index"
 end
